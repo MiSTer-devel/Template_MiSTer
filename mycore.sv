c1c01fd399b1be73b4c5637d0a9de4118d8527fd
sorgelig <pour.garbage@gmail.com>
2020-11-06 00:02:26 +0800

    Add signal for forcing VGA scaler.

--- mycore.sv
+++ mycore.sv
@@ -47,6 +47,7 @@ module emu
 	output        VGA_DE,    // = ~(VBlank | HBlank)
 	output        VGA_F1,
 	output [1:0]  VGA_SL,
+	output        VGA_SCALER, // Force VGA scaler
 
 	/*
 	// Use framebuffer from DDRAM (USE_FB=1 in qsf)
@@ -159,6 +160,7 @@ assign {DDRAM_CLK, DDRAM_BURSTCNT, DDRAM_ADDR, DDRAM_DIN, DDRAM_BE, DDRAM_RD, DD
 
 assign VGA_SL = 0;
 assign VGA_F1 = 0;
+assign VGA_SCALER = 0;
 
 assign AUDIO_S = 0;
 assign AUDIO_L = 0;
