0b81628d795a5377f97734e1c319fe4125131532
Martin Donlon <wickerwaka@users.noreply.github.com>
2022-02-19 23:44:28 -0800

    Support 256 phases in upscaler (#51)
    Set FRAC to 8.
    Add an additional stage in the horizontal upscale and increase division by 2 bits.

--- sys/sys_top.v
+++ sys/sys_top.v
@@ -295,7 +295,7 @@ reg [31:0] cfg_custom_p2;
 reg  [4:0] vol_att;
 initial vol_att = 5'b11111;
 
-reg  [9:0] coef_addr;
+reg  [11:0] coef_addr;
 reg  [8:0] coef_data;
 reg        coef_wr = 0;
 
@@ -424,7 +424,7 @@ always@(posedge clk_sys) begin
 			if(cmd == 'h27) VSET <= io_din[11:0];
 			if(cmd == 'h2A) begin
 				if(cnt[0]) {coef_wr,coef_data} <= {1'b1,io_din[8:0]};
-				else coef_addr <= io_din[9:0];
+				else coef_addr <= io_din[11:0];
 			end
 			if(cmd == 'h2B) scaler_flt <= io_din[2:0];
 			if(cmd == 'h37) {FREESCALE,HSET} <= {io_din[15],io_din[11:0]};
@@ -647,7 +647,7 @@ ascal
 `ifdef MISTER_DOWNSCALE_NN
 	.DOWNSCALE_NN("true"),
 `endif
-	.FRAC(6),
+	.FRAC(8),
 	.N_DW(128),
 	.N_AW(28)
 )
